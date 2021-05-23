1a77ec09aa7ee5ea982907b1f9908c6cc5d5d6e8
samuelindraw <36619720+samuelindraw@users.noreply.github.com>
2021-05-23 21:54:47 +0700

    first commit
    Uploading new one

---
+++ Uploadimgtest/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Uploadimgtest.Models;
+
+namespace Uploadimgtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:53944");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync("/api/User").Result;
+                string stringData = response.Content.ReadAsStringAsync().Result;
+                if (response.StatusCode == HttpStatusCode.BadRequest || response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    return Redirect("/Home/UnAuthorized");
+                }
+
+                List<User> data = JsonConvert.DeserializeObject<List<User>>(stringData);
+                //Aku lupa ini buat apa ? 
+                //var test = data.FirstOrDefault().Tenant.TenantName;
+                //HttpContext.Session.SetString("datatenant", test);
+                //var datatenant = HttpContext.Session.GetString("datatenant");
+                return View(data);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
